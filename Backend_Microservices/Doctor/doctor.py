a45ce0fd59ea4facf152344b9ff4a2c1bd7bc7ee
JosukeD <108534678+JosukeD@users.noreply.github.com>
2024-05-17 16:46:10 -0500

    listo 2

--- Backend_Microservices/Doctor/doctor.py
+++ Backend_Microservices/Doctor/doctor.py
@@ -9,3 +9,10 @@ def get_doctors():
     doctors = Doctor.query.all()
     result = [{'id': doc.id, 'first_name': doc.first_name, 'last_name': doc.last_name, 'age': doc.age, 'gender': doc.gender, 'years_of_experience': doc.years_of_experience} for doc in doctors]
     return jsonify(result), 200
+
+@doctor_bp.route('/post', methods=['POST'])
+def create_doctor():
+    data = request.get_json()
+    first_name = data['first_name']
+    last_name = data['last_name']
+    age = data['age']
